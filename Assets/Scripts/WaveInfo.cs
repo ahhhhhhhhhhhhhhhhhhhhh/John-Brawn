052b680a2cc3dd71a528a422ff99a228c5d97821
icemoon97 <julianbaldwin97@gmail.com>
2019-07-19 13:04:36 -0700

    added waveinfo serializable class to store info about waves

---
+++ Assets/Scripts/WaveInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class WaveInfo {
+    public float timeBetweenWaves;
+    public float initialDelay;
+    public float spawnDelay = 0.1f; //time between each spawn within a wave
+    public int[] wavePattern; //number of enemies to spawn on each wave (waveNum = index)
+}
