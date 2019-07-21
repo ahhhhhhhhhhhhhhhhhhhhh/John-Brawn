85d3c630ddfc059ccc41e060000d0661f706359a
icemoon97 <julianbaldwin97@gmail.com>
2019-07-20 21:39:34 -0700

    improved wave controller
    waves are now more customizable
    added no-repeats rng system
    added wave countdown timer

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveController : MonoBehaviour {
 
     public bool on = true; //way to turn wave spawning off for testing purposes
 
     [Header("Wave Properties")]
-    public WaveInfo waveInfo;
+    public WaveInfo[] waves;
 
     private float timer;
     private int waveNum = 0;
@@ -19,10 +20,12 @@ public class WaveController : MonoBehaviour {
     public Transform spawnpointParent;
     private Transform[] spawnpoints;
 
+    public Text waveCountdown;
+
     // Use this for initialization
     void Start ()
     {
-        timer = waveInfo.initialDelay;
+        timer = waves[0].delay;
 
         spawnpoints = new Transform[spawnpointParent.childCount];
         for (int i = 0; i < spawnpoints.Length; i++)
@@ -34,35 +37,43 @@ public class WaveController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (timer <= 0 && on && waveNum < waveInfo.wavePattern.Length)
+        if (timer <= 0 && on && waveNum < waves.Length)
         {
             StartCoroutine(SpawnWave());
             waveNum++;
-            timer = waveInfo.timeBetweenWaves;
+            if (waveNum < waves.Length)
+            {
+                timer = waves[waveNum].delay;
+            }
         }
 
         timer -= Time.deltaTime;
+        if (timer > 0) //timer being negative means all waves are done
+        {
+            waveCountdown.text = timer.ToString();
+        }
 	}
 
     //spawns next wave on enemies from wave pattern
     IEnumerator SpawnWave() 
     {
-        int numToSpawn = waveInfo.wavePattern[waveNum];
+        int numToSpawn = waves[waveNum].numZombies;
+        float spawnDelay = waves[waveNum].spawnDelay;
 
         for (int i = 0; i < numToSpawn / spawnpoints.Length; i++)
         {
             foreach (Transform spawnpoint in spawnpoints)
             {
                 SpawnEnemy(spawnpoint, zombiePrefab);
-                yield return new WaitForSeconds(waveInfo.spawnDelay);
+                yield return new WaitForSeconds(spawnDelay);
             }
         }
 
+        int[] rand = getShuffledArray(spawnpoints.Length);
         for (int i = 0; i < numToSpawn % spawnpoints.Length; i++)
         {
-            int rand = Random.Range(0, spawnpoints.Length);
-            SpawnEnemy(spawnpoints[rand], zombiePrefab);
-            yield return new WaitForSeconds(waveInfo.spawnDelay);
+            SpawnEnemy(spawnpoints[rand[i]], zombiePrefab);
+            yield return new WaitForSeconds(spawnDelay);
         }
     }
 
@@ -70,5 +81,24 @@ public class WaveController : MonoBehaviour {
     {
         GameObject enemy = Instantiate(enemyType, spawnpoint.position, spawnpoint.rotation);
         enemy.transform.parent = enemies;
+    }
+
+    //returns shuffled array of length n
+    private int[] getShuffledArray(int n)
+    {
+        System.Random rand = new System.Random();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = i;
+        }
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int index = rand.Next(n);
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+        return arr;
     }
 }
--- Assets/Scripts/WaveInfo.cs
+++ Assets/Scripts/WaveInfo.cs
@@ -2,8 +2,9 @@
 
 [System.Serializable]
 public class WaveInfo {
-    public float timeBetweenWaves;
-    public float initialDelay;
+    public float delay; //time before wave starts
     public float spawnDelay = 0.1f; //time between each spawn within a wave
-    public int[] wavePattern; //number of enemies to spawn on each wave (waveNum = index)
+
+    public int numZombies;
+    //other zombie types
 }
