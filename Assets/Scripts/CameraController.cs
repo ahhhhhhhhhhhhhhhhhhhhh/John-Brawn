2e968247f5bb26edaf33ac1b4072100159ec4990
icemoon97 <julianbaldwin97@gmail.com>
2019-07-26 16:29:47 -0700

    camera is now properly locked to only viewing the map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,18 +9,23 @@ public class CameraController : MonoBehaviour {
     private float maxSize; //max size is set automatically by gridcontroller
     public float minSize;
     public float movementSpeed;
+    public float scrollSpeed;
+
+    [Header("Unity Setup")]
+    public GridController grid;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         camera = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+    {
         //camera size adjustment
         float cameraSize = camera.orthographicSize;
-        cameraSize -= Input.mouseScrollDelta.y;
+        cameraSize -= Input.mouseScrollDelta.y * scrollSpeed;
 
         cameraSize = Mathf.Min(maxSize, cameraSize);
         cameraSize = Mathf.Max(minSize, cameraSize);
@@ -29,18 +34,20 @@ public class CameraController : MonoBehaviour {
 
         //camera movement
         Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        movementVector = movementVector * movementSpeed * Time.deltaTime * (cameraSize / 2f);
+        movementVector = movementVector * movementSpeed * Time.deltaTime * (cameraSize / 5f);
+        Vector3 futurePosition = camera.transform.position + movementVector;
 
-        if (camera.transform.position.y + movementVector.y < cameraSize - 0.5f)
+        if (futurePosition.y < cameraSize - 0.5f || futurePosition.y > (grid.getHeight() - 0.5f) - cameraSize)
         {
-            movementVector.y = 0;
+            futurePosition.y = camera.transform.position.y;
         }
-        if (camera.transform.position.y + movementVector.y > (maxSize * 2 - 0.5f) - cameraSize)
+        float cameraWidth = cameraSize * camera.aspect;
+        if (futurePosition.x < cameraWidth - 0.5f || futurePosition.x > (grid.getWidth() - 0.5f) - cameraWidth)
         {
-            movementVector.y = 0;
+            futurePosition.x = camera.transform.position.x;
         }
 
-        camera.transform.position = camera.transform.position + movementVector;
+        camera.transform.position = futurePosition;
     }
 
     //adjusts boundaries for the camera's size, set by the gridcontroller
