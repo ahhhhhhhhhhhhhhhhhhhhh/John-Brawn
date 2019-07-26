78bb57a92ab9f4277494368a8faae79cec401d21
nicho <nicholasjohnstonbradley@gmail.com>
2019-07-25 21:12:33 -0700

    Initial level select menu creation

---
+++ Assets/Scripts/LevelSelectorCity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectorCity : MonoBehaviour {
+
+    public string city_name;
+    public int price;
+    public int reputation;
+    public int state;
+    public int num_waves;
+
+    public Sprite[] images;
+
+    public GameObject PopupPrefab;
+
+	// Use this for initialization
+	void Start () {
+        name = city_name;
+        setState(state);
+        GetComponent<Button>().onClick.AddListener(new UnityEngine.Events.UnityAction(display_properties));
+	}
+	
+    public void setState(int new_state)
+    {
+        state = new_state;
+        GetComponent<Image>().sprite = images[state];
+    }
+
+    public void display_properties()
+    {
+        if (state == 1)
+        {
+            GameObject popup = Instantiate(PopupPrefab);
+            popup.transform.position = transform.position;
+            popup.GetComponent<PopupCityInspector>().city = this;
+            GameObject canvas = GetComponentInParent<Canvas>().gameObject;
+            popup.transform.SetParent(canvas.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PopupCityInspector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupCityInspector : MonoBehaviour {
+
+    // Use this for initialization
+
+    public LevelSelectorCity city;
+    LevelSelector level_selector;
+
+	void Start () {
+        level_selector = GameObject.Find("Level Selector").GetComponent<LevelSelector>();
+        renderProperties();
+        GameObject startButton = transform.Find("Play Button").gameObject;
+        startButton.GetComponent<Button>().onClick.AddListener(new UnityEngine.Events.UnityAction(start_game));
+
+        GameObject quitButton = transform.Find("Quit Button").gameObject;
+        quitButton.GetComponent<Button>().onClick.AddListener(new UnityEngine.Events.UnityAction(quit));
+
+
+    }
+
+    void start_game()
+    {
+        level_selector.loadLevel(city.city_name);
+    }
+
+    void quit()
+    {
+        Destroy(gameObject);
+    }
+
+    public void renderProperties()
+    {
+        Text prop_text = transform.Find("Info").GetComponent<Text>();
+        Text title = transform.Find("Title").GetComponent<Text>();
+        string properties = "Payroll: " + city.price + "\nLength: " + city.num_waves + " days\nReputation: Level " + city.reputation;
+        prop_text.text = properties;
+        title.text = city.city_name;
+    }
+	
+}
