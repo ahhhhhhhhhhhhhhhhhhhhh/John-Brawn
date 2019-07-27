e824ab99c2a0626636f8a4a3a509f59549716077
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2019-07-26 21:49:56 -0700

    Super basic pause function
    Triggered by p or escape
    
    -hopefully no merge conflicts-

---
+++ Assets/Pauser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pauser : MonoBehaviour {
+
+    public GameObject panel;
+
+    private bool paused;
+
+	// Use this for initialization
+	void Start () {
+
+        panel.gameObject.SetActive(false);
+        paused = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!paused && (Input.GetKeyDown("p") || Input.GetKeyDown("escape"))) {
+            Time.timeScale = 0;
+            panel.gameObject.SetActive(true);
+        }
+
+        if (paused && (Input.GetKeyDown("p") || Input.GetKeyDown("escape")))
+        {
+            Time.timeScale = 1;
+            panel.gameObject.SetActive(false);
+        }
+
+        //setting paused has to be pulled out so that the previous section can work its stuff
+        if (panel.gameObject.active)
+        {
+            paused = true;
+        }
+        else {
+            paused = false;
+        }
+
+    }
+}
