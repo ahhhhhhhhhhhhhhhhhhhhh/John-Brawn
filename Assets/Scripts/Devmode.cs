8c674b09beb5d7a6526935a838c9181368a84620
icemoon97 <julianbaldwin97@gmail.com>
2019-08-27 02:26:28 -0700

    added a basic devmode panel
    only has an add money cheat right now, but can be easily expanded in the future

---
+++ Assets/Scripts/Devmode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Devmode : MonoBehaviour {
+
+    public GameObject panel;
+
+    private MoneyManager money;
+    private WaveController waveControl;
+    private BuildingManager buildManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        panel.gameObject.SetActive(false);
+
+        money = gameObject.GetComponent<MoneyManager>();
+        waveControl = gameObject.GetComponent<WaveController>();
+        buildManager = gameObject.GetComponent<BuildingManager>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.D))
+        {
+            if (panel.gameObject.activeSelf)
+            {
+                Hide();
+            }
+            else
+            {
+                Show();
+            }
+        }
+	}
+
+    public void Show()
+    {
+        panel.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    public void addMoney(int added)
+    {
+        money.add(added);
+    }
+}
